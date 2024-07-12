8284ed78379b8b62284764e33e42a6896916f778
Lucian Cheng <luciancheng3@gmail.com>
2024-07-12 09:34:21 -0400

    pages

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
               <filter id="goo">
                 <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
                 <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 14 -8" result="goo" />
+                
                 {/* <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" /> */}
                 <feBlend in="SourceGraphic" in2="goo" />
               </filter>
